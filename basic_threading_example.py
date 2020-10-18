68408c4d747e4daab7651530f0c85ff7c4319a7a
Micah Resnick <resnickmicah@gmail.com>
2020-10-18 09:43:57 -0400

    Two threads same function, function-local vars

--- basic_threading_example.py
+++ basic_threading_example.py
@@ -1,15 +1,13 @@
 from threading import Thread
 
-def p_one():
+def print_range():
     for i in range(1,100):
         print(f"Printing {i} in first thread!")
 
-def p_two():
-    for i in range(1,100):
-        print(f"Printing {i} in second thread!")
-
-t1 = Thread(target=p_one)
-t2 = Thread(target=p_two)
+t1 = Thread(target=print_range)
+t1.name = "one"
+t2 = Thread(target=print_range)
+t2.name = "two"
 
 t1.start()
 t2.start()
